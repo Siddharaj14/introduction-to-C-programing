24911407672ca246374c65998ae5e4dcd8df5ebf
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2025-10-14 21:58:52 +0530

    Add files via upload

---
+++ p42. print your name n times.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+char name[n];
+
+printf("Enter name :");
+scanf("%s",name);
+
+printf("Enter n :");
+scanf("%d",&n);
+
+for(int i=0;i<=n;i++)
+{
+printf("%s\n",name);
+}
+}
