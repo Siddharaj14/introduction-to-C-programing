d65978b5d53ac8eb9a9738c740f8bdd83dd0514b
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2025-10-16 19:04:22 +0530

    Add files via upload

---
+++ Lab 3/p38. print sum of 1st n odd no..c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  int i,n,sum=0;
+printf("Enter n :");
+scanf("%d\n",&n);
+
+for(i=1;i<=n,i++);
+
+if(i%2 !=0)
+{
+printf("%d",i);
+sum = sum+i;
+}
+printf("The sum of odd numbers are %d"\n,sum);
+}
+
