b7c7977392985c75b3f31890e9108e7c24207499
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2025-10-16 13:24:01 +0530

    Add files via upload

---
+++ Lab 1/p6  code for converting hours to minute.c
@@ -0,0 +1,21 @@
+// code for converting hours to minutes
+#include <stdio.h>
+int main()
+{
+    int a , b ;
+    printf("enter the number of hours . \n");
+    scanf("%d", &a );
+    b=a*60;
+
+    printf("%d hours is equal to %d miniute", a , b);
+
+    
+    return 0;
+}
+/*
+Output
+
+enter the number of hours . 
+3
+3 hours is equal to 180 miniute
+*/
