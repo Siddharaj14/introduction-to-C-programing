83510cca03dd161ecffabc86b96caf993449add5
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2025-10-16 19:03:57 +0530

    Add files via upload

---
+++ Lab 3/p37. print sum of n natursl no..c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,sum=0;
+    printf("Enter n :");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum = sum+i;
+    }
+        printf("Sum of natural numbers is %d", sum);
+
+}
